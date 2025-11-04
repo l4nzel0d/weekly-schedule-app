588cd43fd5057681426e76d929013431972c465f
l4nzel0d <borisych095@yandex.ru>
2025-11-04 13:29:10 +0000

    add viewing schedule

---
+++ resources/views/schedule/index.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1 class="mb-4">Моё расписание</h1>
+
+    {{-- Переключатель дней недели --}}
+    <ul class="nav nav-tabs mb-3">
+        <li class="nav-item">
+            <a class="nav-link {{ request('day') == 'all' || !request()->has('day') ? 'active' : '' }}" href="{{ route('schedule.index', ['day' => 'all']) }}">Вся неделя</a>
+        </li>
+        @php
+            $days = [1 => 'Пн', 2 => 'Вт', 3 => 'Ср', 4 => 'Чт', 5 => 'Пт', 6 => 'Сб', 7 => 'Вс'];
+        @endphp
+        @foreach ($days as $dayNumber => $dayName)
+            <li class="nav-item">
+                <a class="nav-link {{ request('day') == $dayNumber ? 'active' : '' }}" href="{{ route('schedule.index', ['day' => $dayNumber]) }}">{{ $dayName }}</a>
+            </li>
+        @endforeach
+    </ul>
+
+    {{-- Таблица с расписанием --}}
+    <div class="table-responsive">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">День недели</th>
+                    <th scope="col">Время</th>
+                    <th scope="col">Название</th>
+                    <th scope="col">Описание</th>
+                </tr>
+            </thead>
+            <tbody>
+                @php $lastDay = null; @endphp
+                @forelse ($groupedEntries as $day => $entries)
+                    @foreach ($entries as $entry)
+                        <tr class="schedule-row" data-entry='{{ json_encode($entry) }}'>
+                            <td>
+                                @if ($day !== $lastDay)
+                                    <strong>{{ $days[$day] }}</strong>
+                                @endif
+                            </td>
+                            <td>{{ date('H:i', strtotime($entry->start_time)) }} - {{ date('H:i', strtotime($entry->end_time)) }}</td>
+                            <td>{{ $entry->title }}</td>
+                            <td>{{ $entry->description }}</td>
+                        </tr>
+                        @php $lastDay = $day; @endphp
+                    @endforeach
+                @empty
+                    <tr>
+                        <td colspan="4" class="text-center">На выбранный день записей нет.</td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
+</div>
+@endsection
